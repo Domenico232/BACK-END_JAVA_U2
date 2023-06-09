43081915bda6dca7b1188bd8aef328063a451c25
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-06-09 10:42:23 +0200

    start proj

---
+++ WeekProjSpringII/src/main/java/com/weekproj/springII/WeekProjSpringIiApplication.java
@@ -0,0 +1,13 @@
+package com.weekproj.springII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekProjSpringIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekProjSpringIiApplication.class, args);
+	}
+
+}
---
+++ WeekProjSpringII/src/test/java/com/weekproj/springII/WeekProjSpringIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weekproj.springII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekProjSpringIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
